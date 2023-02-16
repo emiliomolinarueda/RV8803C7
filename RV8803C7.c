7f9535d5003df82995ca33023544e79b1cb772e7
emiliomolinarueda <emiliomolinarueda@outlook.com>
2023-02-15 17:31:31 -0700

    Add files

---
+++ RV8803C7.c
@@ -0,0 +1,372 @@
+/******************************************************************//**
+* @file RV8803C7.cpp
+*
+* @author Casey Quinn
+*
+* @version 1.0
+*
+* Started: 19JUN20
+*
+* Updated: 
+*
+* @brief Source file for RV8803C7 class
+*
+**********************************************************************/
+
+
+#include "RV8803C7.h"  
+
+
+// Convert normal decimal numbers to binary coded decimal
+uint8_t decToBcd(int val)
+{
+  return (uint8_t)( (val/10*16) + (val%10) );
+}
+// Convert binary coded decimal to normal decimal numbers
+int bcdToDec(uint8_t val)
+{
+  return (int)( (val/16*10) + (val%16) );
+}
+
+HAL_StatusTypeDef RV8803C7_set_time (uint8_t sec, uint8_t min, uint8_t hour)
+{
+	uint8_t data[3];
+	data[0] = decToBcd(sec);
+	data[1] = decToBcd(min);
+	data[2] = decToBcd(hour);
+
+    //Make sure data is within range.
+    if((sec > 59) || (min > 59) || (hour > 23))
+    {
+        return(HAL_ERROR);
+    }
+    else
+    {
+    	return(HAL_I2C_Mem_Write(&hi2c1, RV8803C7_I2C_ADRS << 1, 0x00, 1, data, 3, 1000));
+    }
+}
+
+HAL_StatusTypeDef RV8803C7_set_calendar (uint8_t dow, uint8_t dom, uint8_t month, uint8_t year)
+{
+	uint8_t data[4];
+	data[0] = (1 << dow);
+	data[1] = decToBcd(dom);
+	data[2] = decToBcd(month);
+	data[3] = decToBcd(year);
+
+    //Make sure data is within range.
+    if(((dow > 6)) ||
+       ((dom < 1) || (dom > 31)) ||
+       ((month < 1) || (month > 12)) ||
+       (year > 99))
+    {
+        return(HAL_ERROR);
+    }
+    else
+    {
+    	return(HAL_I2C_Mem_Write(&hi2c1, RV8803C7_I2C_ADRS << 1, 0x03, 1, data, 4, 1000));
+    }
+}
+
+HAL_StatusTypeDef RV8803C7_get_time (RV8803C7_time_t* time)
+{
+	uint8_t data[3];
+	HAL_StatusTypeDef rtn_val = HAL_ERROR;
+	rtn_val = HAL_I2C_Mem_Read(&hi2c1, RV8803C7_I2C_ADRS << 1, 0x00, 1, data, 3, 1000);
+	time->seconds = bcdToDec(data[0]);
+	time->minutes = bcdToDec(data[1]);
+	time->hours = bcdToDec(data[2]);
+	return(rtn_val);
+}
+
+HAL_StatusTypeDef RV8803C7_get_calendar (RV8803C7_calendar_t* calendar)
+{
+	uint8_t data[4];
+	HAL_StatusTypeDef rtn_val = HAL_ERROR;
+	rtn_val = HAL_I2C_Mem_Read(&hi2c1, RV8803C7_I2C_ADRS << 1, 0x03, 1, data, 4, 1000);
+	calendar->day = (uint8_t)(log2((float)data[0]));
+	calendar->date = bcdToDec(data[1]);
+	calendar->month = bcdToDec(data[2]);
+	calendar->year = bcdToDec(data[3]);
+	return(rtn_val);
+}
+
+
+
+/**********************************************************//**
+* Sets the time on RV8803C7
+* Struct data is in integrer format, not BCD.  Fx will convert
+* to BCD for you.
+*
+* On Entry:
+*     @param[in] time - struct cotaining time data; 
+*
+* On Exit:
+*     @return return value = 0 on success, non-0 on failure
+*
+* Example:
+* @code
+* 
+* //instantiate rtc object
+* RV8803C7 rtc(D14, D15); 
+* 
+* //time = 14:00:00
+* RV8803C7_time_t time = {14, 0, 0}
+* uint16_t rtn_val;
+*
+* rtn_val = rtc.set_time(time);
+*
+* @endcode
+**************************************************************/
+/*uint16_t RV8803C7::set_time(RV8803C7_time_t time)
+{
+    uint8_t data[] = {0,0,0,0};
+    uint8_t data_length = 0;
+    uint8_t max_hour = 24;
+    
+    data[data_length++] = SECONDS;
+    data[data_length++] = uchar_2_bcd(time.seconds);
+    data[data_length++] = uchar_2_bcd(time.minutes);
+    
+    //format Hours register
+    data[data_length] = uchar_2_bcd(time.hours);
+    
+    max_hour = max_hour - 1;
+    
+    data_length++;
+    
+    //Make sure data is within range.  
+    if((time.seconds > 59) || (time.minutes > 59) || (time.hours > max_hour))
+    {
+        return(1);
+    }
+    else
+    {   
+        //printf("w_adrs: %d, data_length: %d\r\n", w_adrs, data_length);
+        return(write(w_adrs,(const char*) data, data_length));
+    }
+}
+*/
+
+/**********************************************************//**
+* Sets the calendar on RV8803C7
+*
+* On Entry:
+*     @param[in] calendar - struct cotaining calendar data 
+*
+* On Exit:
+*     @return return value = 0 on success, non-0 on failure
+*
+* Example:
+* @code
+* 
+* //instantiate rtc object
+* RV8803C7 rtc(D14, D15); 
+* 
+* //see datasheet for calendar format
+* RV8803C7_calendar_t calendar = {1, 1, 1, 0}; 
+* uint16_t rtn_val;
+*
+* rtn_val = rtc.set_calendar(calendar);
+*
+* @endcode
+**************************************************************/
+/*uint16_t RV8803C7::set_calendar(RV8803C7_calendar_t calendar)
+{
+    uint8_t data[] = {0,0,0,0,0};
+    uint8_t data_length = 0;
+
+    data[data_length++] = DAY;
+    data[data_length++] = (1 << calendar.day);
+    data[data_length++] = uchar_2_bcd(calendar.date);
+    data[data_length++] = uchar_2_bcd(calendar.month);
+    data[data_length++] = uchar_2_bcd(calendar.year);
+
+    //Make sure data is within range.  
+    if(((calendar.day < 1) || (calendar.day > 7)) || 
+       ((calendar.date < 1) || (calendar.date > 31)) || 
+       ((calendar.month < 1) || (calendar.month > 12)) || 
+       (calendar.year > 99))
+    {
+        return(1);
+    }
+    else
+    {
+        return(write(w_adrs,(const char*) data, data_length));
+    }
+}
+*/
+
+
+/**********************************************************//**
+* Gets the time on RV8803C7
+*
+* On Entry:
+*     @param[in] time - pointer to struct for storing time data
+*
+* On Exit:
+*     @param[out] time - contains current integrer rtc time 
+*                        data
+*     @return return value = 0 on success, non-0 on failure
+*
+* Example:
+* @code
+* 
+* //instantiate rtc object
+* RV8803C7 rtc(D14, D15); 
+* 
+* //time = 14:00:00
+* RV8803C7_time_t time = {14, 0, 0} 
+* uint16_t rtn_val;
+*
+* rtn_val = rtc.get_time(&time);
+*
+* @endcode
+**************************************************************/
+/*uint16_t RV8803C7::get_time(RV8803C7_time_t* time)
+{
+    uint16_t rtn_val = 1;
+    uint8_t data[3];
+    
+    data[0] = SECONDS;
+    rtn_val = write(w_adrs, (const char*) data, 1);
+    
+    if(!rtn_val)
+    {
+        rtn_val = read(r_adrs,(char *) data, 3);
+        
+        time->seconds = bcd_2_uchar(data[0]);
+        time->minutes = bcd_2_uchar(data[1]);
+        time->hours = bcd_2_uchar((data[2]&0x3F));
+    } 
+  
+    return(rtn_val);
+}
+*/
+
+/**********************************************************//**
+* Gets the calendar on RV8803C7
+*
+* On Entry:
+*     @param[in] calendar - pointer to struct for storing 
+*                           calendar data
+*
+* On Exit:
+*     @param[out] calendar - contains current integer rtc 
+*                            calendar data
+*     @return return value = 0 on success, non-0 on failure
+*
+* Example:
+* @code
+* 
+* //instantiate rtc object
+* RV8803C7 rtc(D14, D15); 
+* 
+* //see datasheet for calendar format
+* RV8803C7_calendar_t calendar = {1, 1, 1, 0}; 
+* uint16_t rtn_val;
+*
+* rtn_val = rtc.get_calendar(&calendar);
+*
+* @endcode
+**************************************************************/
+/*uint16_t RV8803C7::get_calendar(RV8803C7_calendar_t* calendar)
+{
+    uint16_t rtn_val = 1;
+    uint8_t data[4];
+    
+    data[0] = DAY;
+    rtn_val = write(w_adrs, (const char*) data, 1);
+    
+    if(!rtn_val)
+    {
+        rtn_val = read(r_adrs,(char *) data, 4);
+        
+        calendar->day = (uint8_t)(log2((float)data[0]));
+        calendar->date = bcd_2_uchar(data[1]);
+        calendar->month = bcd_2_uchar((data[2]&0x1F));
+        calendar->year = bcd_2_uchar(data[3]);
+    } 
+  
+    return(rtn_val);
+}
+*/
+
+
+/**********************************************************//**
+* Get epoch time based on current RTC time and date.  
+* RV8803C7 must be configured and running before this fx is 
+* called
+*
+* On Entry:
+*
+* On Exit:
+*     @return return value = epoch time
+*
+* Example:
+* @code
+* 
+* //instantiate rtc object
+* RV8803C7 rtc(D14, D15); 
+* 
+* time_t epoch_time; 
+*
+* epoch_time = rtc.get_epoch();
+*
+* @endcode
+**************************************************************/
+time_t RV8803C7_get_epoch(void)
+{
+    //system vars
+    struct tm sys_time;
+    
+    //RTC vars
+    RV8803C7_time_t rtc_time = {0,0,0};
+    RV8803C7_calendar_t rtc_calendar = {0,0,0,0};
+    
+    RV8803C7_get_calendar(&rtc_calendar);
+    RV8803C7_get_time(&rtc_time);
+    
+    sys_time.tm_wday = rtc_calendar.day;
+    sys_time.tm_mday = rtc_calendar.date;
+    sys_time.tm_mon = rtc_calendar.month - 1;
+    sys_time.tm_year = rtc_calendar.year + 100;
+    
+    sys_time.tm_hour = rtc_time.hours;
+    sys_time.tm_min = rtc_time.minutes;
+    sys_time.tm_sec = rtc_time.seconds;
+    
+    if(rtc_time.seconds == 59){
+        get_time(&rtc_time);
+        
+        sys_time.tm_hour = rtc_time.hours;
+        sys_time.tm_min = rtc_time.minutes;
+        sys_time.tm_sec = rtc_time.seconds;
+            
+    }
+    
+    //make epoch time
+    return(mktime(&sys_time));
+}
+
+/*uint32_t RV8803C7::get_hundredth_secs(void)
+{
+    uint16_t rtn_val = 1;
+    uint32_t hundredth_sec = 0;
+    uint8_t data[1];
+    
+    data[0] = SECONDS_100TH;
+    rtn_val = write(w_adrs, (const char*) data, 1);
+    
+    if(!rtn_val)
+    {
+        rtn_val = read(r_adrs,(char *) data, 1);
+        
+        hundredth_sec = bcd_2_uchar(data[0]);
+        
+    } 
+  
+    return(hundredth_sec);
+}
+*/
+
---
+++ RV8803C7.h
@@ -0,0 +1,157 @@
+/******************************************************************//**
+* @file RV8803C7.h
+*
+* @author Casey Quinn
+*
+* @version 1.0
+*
+* Started: 19JUN20
+*
+* Updated: 
+*
+* @brief Header file for RV8803C7 class
+*
+**********************************************************************/
+
+
+#ifndef RV8803C7_H
+#define RV8803C7_H
+
+#include "main.h"
+#include "stm32wbxx_hal.h"
+#include <time.h>
+
+#define _M_LN2  0.693147180559945309417 // Natural log of 2
+#define log2(x) (log(x)/_M_LN2)
+
+#define RV8803C7_I2C_ADRS 0x32
+
+extern I2C_HandleTypeDef hi2c1;
+
+/*
+#define AM_PM     (1 << 5) 
+#define MODE      (1 << 6)
+#define DY_DT     (1 << 6)
+#define ALRM_MASK (1 << 7)
+
+//control register bit masks
+#define A1IE  (1 << 0)
+#define A2IE  (1 << 1)
+#define INTCN (1 << 2)
+#define RS1   (1 << 3)
+#define RS2   (1 << 4)
+#define CONV  (1 << 5)
+#define BBSQW (1 << 6)
+#define EOSC  (1 << 7)
+
+//status register bit masks
+#define A1F     (1 << 0)
+#define A2F     (1 << 1)
+#define BSY     (1 << 2)
+#define EN32KHZ (1 << 3)
+#define OSF     (1 << 7)
+*/  
+
+/**
+* RV8803C7_time_t - Struct for containing time data.
+* 
+* Members:
+*
+* - uint32_t seconds - Use decimal value. Member fx's convert to BCD
+*
+* - uint32_t minutes - Use decimal value. Member fx's convert to BCD
+*
+* - uint32_t hours   - Use decimal value. Member fx's convert to BCD
+*
+*/
+typedef struct
+{
+    uint8_t seconds;
+    uint8_t minutes;
+    uint8_t hours;
+
+}RV8803C7_time_t;
+
+
+/**
+* RV8803C7_calendar_t - Struct for containing calendar data.
+* 
+* Members:
+*
+* - uint32_t day   - Use decimal value. Member fx's convert to BCD
+*
+* - uint32_t date  - Use decimal value. Member fx's convert to BCD
+*
+* - uint32_t month - Use decimal value. Member fx's convert to BCD
+*
+* - uint32_t year  - Use decimal value. Member fx's convert to BCD
+*/
+typedef struct
+{
+    uint8_t day;
+    uint8_t date;
+    uint8_t month;
+    uint8_t year;
+}RV8803C7_calendar_t;
+
+
+/**
+* RV8803C7_cntl_stat_t - Struct for containing control and status 
+* register data.
+* 
+* Members:
+*
+* - uint8_t control - Binary data for read/write of control register 
+*
+* - uint8_t status  - Binary data  for read/write of status register 
+*/
+typedef struct
+{
+    uint8_t control; 
+    uint8_t status; 
+}RV8803C7_cntl_stat_t;
+        
+                
+// Convert normal decimal numbers to binary coded decimal
+uint8_t decToBcd(int val);
+
+// Convert binary coded decimal to normal decimal numbers
+int bcdToDec(uint8_t val);
+
+HAL_StatusTypeDef RV8803C7_set_time (uint8_t sec, uint8_t min, uint8_t hour);
+
+HAL_StatusTypeDef RV8803C7_set_calendar (uint8_t dow, uint8_t dom, uint8_t month, uint8_t year);
+
+HAL_StatusTypeDef RV8803C7_get_time (RV8803C7_time_t* time);
+
+HAL_StatusTypeDef RV8803C7_get_calendar (RV8803C7_calendar_t* calendar);
+
+
+    
+/**********************************************************//**
+* Get epoch time based on current RTC time and date.
+* RV8803C7 must be configured and running before this fx is
+* called
+*
+* On Entry:
+*
+* On Exit:
+*     @return return value = epoch time
+*
+* Example:
+* @code
+*
+* //instantiate rtc object
+* RV8803C7 rtc(D14, D15);
+*
+* time_t epoch_time;
+*
+* epoch_time = rtc.get_epoch();
+*
+* @endcode
+**************************************************************/
+time_t RV8803C7_get_epoch(void);
+        
+//uint32_t get_hundredth_secs(void);
+        
+#endif /* RV8803C7_H*/
